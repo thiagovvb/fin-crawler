9de09b5b3814ce3d51535d3eb678ab1b2721e104
Thiago Batista <thiagovvb@gmail.com>
2026-02-06 20:38:27 -0300

    feat: main finalizado

--- src/main.py
+++ src/main.py
@@ -2,14 +2,23 @@ from engine import ScraperEngine
 from parser import PageParser
 import pandas as pd
 import csv
+import sys
 
-scraper = ScraperEngine()
-parser = PageParser()
-scraper.start_navigation()
-scraper.apply_filter("Brazil")
-data = []
-for page in scraper.get_next_page():
-    data.extend(parser.extract_table_data(page))
+def main():
 
-df = pd.DataFrame(data)
-df.to_csv("out.csv", index=False,quoting=csv.QUOTE_ALL)
+    filter = sys.argv[1]
+
+    scraper = ScraperEngine()
+    parser = PageParser()
+
+    scraper.start_navigation()
+    scraper.apply_filter(filter)
+
+    data = []
+    for page in scraper.get_next_page():
+        data.extend(parser.extract_table_data(page))
+
+    df = pd.DataFrame(data)
+    df.to_csv("output.csv", index=False,quoting=csv.QUOTE_ALL)
+
+main()
